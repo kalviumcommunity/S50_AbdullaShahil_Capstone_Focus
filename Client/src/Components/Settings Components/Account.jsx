77e418f6bc8bfe970826ed7ab891275b568ada8a
Abdulla Shahil <abdullashahil0077@gmail.com>
2024-04-08 00:53:29 +0530

    Settings page UI

--- Client/src/Components/Settings Components/Account.jsx
+++ Client/src/Components/Settings Components/Account.jsx
@@ -1,73 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react'
 
 function Account() {
-  const [isDropdownExpanded, setIsDropdownExpanded] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsDropdownExpanded(!isDropdownExpanded);
-  };
-
   return (
-    <div className="col-span-8 overflow-hidden rounded-xl sm:bg-gray-50 sm:px-8 sm:shadow">
-      <div className="pt-4">
-        <h1 className="py-2 text-2xl font-semibold">Account settings</h1>
-      </div>
-      <hr className="mt-4 mb-8" />
-      <p className="py-2 text-xl font-semibold">Email Address</p>
-      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-        <p className="text-gray-600">Your email address is <strong>john.doe@company.com</strong></p>
-        <button className="inline-flex text-sm font-semibold text-blue-600 underline decoration-2">Change</button>
-      </div>
-      <hr className="mt-4 mb-8" />
-      <p className="py-2 text-xl font-semibold">Password</p>
-      <div className="flex items-center">
-        <div className="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3">
-          <label htmlFor="login-password">
-            <span className="text-sm text-gray-500">Current Password</span>
-            <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-              <input type="password" id="login-password" className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="***********" />
-            </div>
-          </label>
-          <label htmlFor="new-login-password">
-            <span className="text-sm text-gray-500">New Password</span>
-            <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-              <input type="password" id="new-login-password" className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="***********" />
-            </div>
-          </label>
-        </div>
-        <svg xmlns="http://www.w3.org/2000/svg" className="mt-5 ml-2 h-6 w-6 cursor-pointer text-sm font-semibold text-gray-600 underline decoration-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
-        </svg>
-      </div>
-      <button className="mt-4 rounded-lg gradient1 px-4 py-2 text-white">Change Password</button>
-      <hr className="mt-4 mb-8" />
 
-      <div className="mb-10">
-        <p className="py-2 text-xl font-semibold">Delete Account</p>
-      </div>
+<div className="col-span-8 overflow-hidden rounded-xl sm:bg-gray-50 sm:px-8 sm:shadow">
+          <div className="pt-4">
+            <h1 className="py-2 text-2xl font-semibold">Account settings</h1>
+          </div>
+          <hr className="mt-4 mb-8" />
+          <p className="py-2 text-xl font-semibold">Email Address</p>
+          <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+            <p className="text-gray-600">Your email address is <strong>john.doe@company.com</strong></p>
+            <button className="inline-flex text-sm font-semibold text-blue-600 underline decoration-2">Change</button>
+          </div>
+          <hr className="mt-4 mb-8" />
+          <p className="py-2 text-xl font-semibold">Password</p>
+          <div className="flex items-center">
+            <div className="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3">
+              <label htmlFor="login-password">
+                <span className="text-sm text-gray-500">Current Password</span>
+                <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+                  <input type="password" id="login-password" className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="***********" />
+                </div>
+              </label>
+              <label htmlFor="new-login-password">
+                <span className="text-sm text-gray-500">New Password</span>
+                <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+                  <input type="password" id="new-login-password" className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="***********" />
+                </div>
+              </label>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" className="mt-5 ml-2 h-6 w-6 cursor-pointer text-sm font-semibold text-gray-600 underline decoration-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
+            </svg>
+          </div>
+          <button className="mt-4 rounded-lg gradient1 px-4 py-2 text-white">Change Password</button>
+          <hr className="mt-4 mb-8" />
 
-      {/* Adding aria-expanded to reflect the dropdown state */}
-      <input
-        type="checkbox"
-        className="hidden"
-        id="account-dropdown"
-        aria-expanded={isDropdownExpanded}
-      />
-      <label
-        htmlFor="account-dropdown"
-        className="cursor-pointer py-2 text-base text-blue-600 underline"
-        onClick={toggleDropdown}
-      >
-        View/Delete Account Options
-      </label>
-      {/* Your dropdown content here */}
-      {isDropdownExpanded && (
-        <div>
-          {/* Dropdown content */}
+          <div className="mb-10">
+            <p className="py-2 text-xl font-semibold">Delete Account</p>
+
+          </div>
         </div>
-      )}
-    </div>
-  );
+  )
 }
 
-export default Account;
+export default Account
