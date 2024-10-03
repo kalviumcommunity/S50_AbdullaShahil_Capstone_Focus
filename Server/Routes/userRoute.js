45cf4919028149c3b797b90a1c426c809adab902
Abdulla Shahil <141110991+abdullashahil@users.noreply.github.com>
2024-10-03 02:01:46 -0700

    Update userRoute.js

--- Server/Routes/userRoute.js
+++ Server/Routes/userRoute.js
@@ -1,75 +1,517 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const express = require("express");
+const router = express.Router();
+const mongoose = require('mongoose');
+const userModel = require("../Models/userModel");
+const Joi = require("joi");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const profileModel = require("../Models/profileModel");
+const postModel = require("../Models/postModel");
+const articleModel = require("../Models/articleModel");
+const communityModel = require("../Models/communityModel");
 
-require('dotenv').config();
+const saltRounds = 10;
 
-const Profile = require('./Models/profileModel'); 
-const User = require('./Models/userModel'); 
+const { Cookie } = require("express-session");
 
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+require('dotenv').config()
 
-const generateToken = (user) => {
-    return jwt.sign({ id: user._id, name: user.name }, process.env.SECRET_KEY, { expiresIn: "5h" });
+router.use(express.json());
+
+const generateToken = (id) => {
+    return jwt.sign({ id: id }, process.env.SECRET_KEY, { expiresIn: "6h" })
 }
 
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://s50-abdullashahil-capstone-focus.onrender.com/auth/google/callback",
-    passReqToCallback: true
-},
-async function (request, accessToken, refreshToken, profile, done) {
+const userJoiSchema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    profile_img: Joi.string(),
+    about: Joi.string(),
+    accountType: Joi.string(),
+
+});
+
+const putUserJoiSchema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30),
+    email: Joi.string().email(),
+    profile_img: Joi.string(),
+    password: Joi.string(),
+    newPassword: Joi.string(),
+    about: Joi.string(),
+    interests: Joi.array()
+}).min(1);
+
+
+const patchUserJoiSchema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30),
+    email: Joi.string().email(),
+    password: Joi.string(),
+    profile_img: Joi.string(),
+    about: Joi.string(),
+    interests: Joi.array()
+}).min(1);
+
+function validateUser(req, res, next) {
+    const { error } = userJoiSchema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+    next();
+}
+
+function validatePutUser(req, res, next) {
+    const { error } = putUserJoiSchema.validate(req.body);
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+    next();
+}
+
+function validatePatchUser(req, res, next) {
+    const { error } = patchUserJoiSchema.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+
+    next();
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.token || req.headers["x-access-token"] || req.body.token;
+
+    if (!token) {
+        return res.status(401).json({ error: "Unauthorized: Token is not provided" });
+    }
+
     try {
-        let profileDoc = await Profile.findOne({ id: profile.id });
-        let userDoc = null;
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.decoded = decoded.id;
 
-        if (!profileDoc) {
-            profileDoc = new Profile({
-                id: profile.id,
-                name: profile.displayName,
-                profile_img: profile.picture,
-                email: profile.email,
-            });
-            
-            userDoc = new User({
-                id: profile.id,
-                name: profile.displayName,
-                email: profile.email,
-                profile: profileDoc._id,
-                accountType: "Google",
-            });
-            
-            await profileDoc.save();
-            await userDoc.save();
+        console.log("Token on login present")
+        next();
+    } catch (error) {
+        return res.status(401).json({ error: "Unauthorized: Invalid token" });
+    }
+};
 
-            
-        } else {
-            userDoc = await User.findOne({ profile: profileDoc._id });
+// VALIDATE TOKEN
+router.post("/tokenvalidate", verifyToken, (req, res) => {
+    res.status(200).json({ valid: true });
+});
+
+
+// GET all users
+router.get("/", async (req, res) => {
+    try {
+        const data = await userModel.find();
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "500-Internal server error" });
+    }
+});
+
+// GET all profiles
+router.get("/profiles", async (req, res) => {
+    try {
+        const users = await userModel.find({}, 'profile');
+        const profileIds = users.map(user => user.profile);
+
+        const profiles = await profileModel.find({ _id: { $in: profileIds } })
+            .select('_id name profile_img ')
+            .lean();
+
+        res.json(profiles);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "500-Internal server error" });
+    }
+});
+
+
+
+// GET users for suggestion
+router.get("/otherUsers", async (req, res) => {
+    try {
+        const data = await profileModel.aggregate([
+            { $sample: { size: 6 } },
+            { $project: { _id: 1, name: 1, profile_img: 1 } }
+        ]).exec();
+
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "500-Internal server error" });
+    }
+});
+
+// GET users for displaying in chat list
+router.get("/list/displayData", async (req, res) => {
+    try {
+        const data = await userModel.aggregate([{ $sample: { size: 5 } }]);
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "500-Internal server error" });
+    }
+});
+
+
+
+// GET each user by ID
+router.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const data = await userModel.findById(id);
+        if (!data) {
+            return res.status(404).json({ error: "User not found" });
         }
 
-        const token = generateToken(userDoc);
+        const profile = await profileModel.findById(data.profile);
 
-        request.res.cookie("token", token, {
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// GET each user's profile by userID
+router.get("/profile/:id", async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const profile = await profileModel.findById(id)
+            .select('_id name email about profile_img communities')
+            .lean();
+
+        res.json(profile);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// GET each user's profile for about, name, interests
+router.get("/profile/get/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+
+        const profile = await profileModel.findById(id)
+            .select('_id name about interests profile_img')
+            .lean();
+
+        res.json(profile);
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+
+const decodetoken = (req, res, next) => {
+    console.log(req.cookies)
+    const token = req.cookies.token || req.headers["x-access-token"] || req.body.token;
+
+    if (!token) {
+        return res.status(401).json({ error: "Unauthorized: Token is not provided" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.decoded = decoded.id;
+
+        console.log("decoded userid", decoded)
+        next();
+    } catch (error) {
+        return res.status(401).json({ error: "Unauthorized: Invalid token" });
+    }
+};
+
+// Token decoding to retrieve userID / profileID
+router.post('/token/getId/:idType', decodetoken, async (req, res) => {
+    try {
+        const { idType } = req.params;
+        const userId = req.decoded;
+
+        if (!userId) {
+            return res.status(400).json({ error: 'User ID not found in token' });
+        }
+
+        // Fetch user once and reuse
+        const user = await userModel.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        if (idType === 'userID') {
+            if (!user.name) {
+                return res.status(400).json({ error: 'User name not found' });
+            }
+
+            return res.status(200).json({ id: userId, name: user.name });
+
+        } else if (idType === 'profileID') {
+            if (!user.profile) {
+                return res.status(400).json({ error: 'Profile ID not found' });
+            }
+
+            return res.status(200).json({ id: user.profile, name: user.name });
+
+        } else {
+            return res.status(400).json({ error: 'Invalid ID type' });
+        }
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+
+// POST a new user
+router.post("/", validateUser, async (req, res) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+        const { name, email, password, picture } = req.body;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        const profile = await profileModel.create([{
+            name: name,
+            email: email,
+            picture: picture
+        }], { session });
+
+        const newUser = new userModel({
+            name: name,
+            email: email,
+            password: hashedPassword,
+            profile: profile[0]._id,
+            accountType: "Normal",
+        });
+
+        await newUser.save({ session });
+
+        await session.commitTransaction();
+        session.endSession();
+
+        const token = generateToken(newUser._id);
+        console.log("token------>", token)
+
+        res.cookie("token", token, {
             httpOnly: true,
             secure: true,
             sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
-        return done(null, token);
-    } catch (err) {
-        return done(err);
+        res.status(201).json({ message: "Signup successful" });
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
     }
-}));
-
-passport.serializeUser(function (user, done) {
-    done(null, user);
 });
 
-passport.deserializeUser(function (user, done) {
-    done(null, user);
+
+
+
+
+// POST REQUEST FOR LOGIN - TO CHECK IF THE EMAIL AND PASSWORD MATCHES
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email }).populate("profile").exec();
+        if (!user) {
+            return res.status(401).json({ error: "Invalid email" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: "Invalid password" });
+        }
+        const token = generateToken(user._id);
+        console.log("token------>", token)
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+
+        // res.status(201).json({ message: "Signup successful" })
+
+        res.status(201).json({ email: user.email, name: user.name, userID: user._id, profileID: user.profile._id });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 });
 
-module.exports = passport;
+// CHANGE PASSWORD
+router.put("/password/change/:id", validatePutUser, async (req, res) => {
+    const userId = req.params.id;
+    const { password, newPassword } = req.body;
+
+    try {
+        const user = await userModel.findById(userId);
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: "Invalid password" });
+        }
+
+        const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+        const updatedUser = await userModel.findByIdAndUpdate(userId, { password: hashedNewPassword }, { new: true });
+
+        console.log(updatedUser)
+        if (!updatedUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+
+        res.json({ message: "Password updated successfully", user: updatedUser });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+
+
+
+// PUT to update a user
+router.put("/:id", validatePutUser, async (req, res) => {
+    const userId = req.params.id;
+    const updatedUserData = req.body;
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        // Update user
+        const updatedUser = await userModel.findByIdAndUpdate(userId, updatedUserData, { new: true }).session(session);
+        if (!updatedUser) {
+            await session.abortTransaction();
+            session.endSession();
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        // Update profile
+        if (updatedUser.profile) {
+            const updateProfile = await profileModel.findByIdAndUpdate(updatedUser.profile._id, updatedUserData, { new: true }).session(session);
+            if (!updateProfile) {
+                await session.abortTransaction();
+                session.endSession();
+                return res.status(404).json({ error: "Profile not found" });
+            }
+        } else {
+            await session.abortTransaction();
+            session.endSession();
+            return res.status(404).json({ error: "Profile not found" });
+        }
+
+        await session.commitTransaction();
+        session.endSession();
+
+        const token = generateToken(updatedUser);
+        res.json({ user: updatedUser, token });
+    } catch (error) {
+        console.error("Error updating user:", error);
+        await session.abortTransaction();
+        session.endSession();
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+
+// PATCH to partially update a user
+router.patch("/:id", validatePatchUser, async (req, res) => {
+    try {
+        const updatedUser = await userModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(updatedUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// PATCH to update profile picture and about
+router.patch("/updateProfile/:id", validatePatchUser, async (req, res) => {
+    try {
+
+        const data = req.body;
+        const profileId = req.params.id
+        const updateProfile = await profileModel.findByIdAndUpdate(profileId, data, { new: true });
+
+        if (!updateProfile) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(updateProfile);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+// DELETE a user
+router.delete("/delete/:id", async (req, res) => {
+    const userId = req.params.id;
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        const user = await userModel.findById(userId).session(session);
+        if (!user) {
+            await session.abortTransaction();
+            session.endSession();
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const profileId = user.profile;
+        const profile = await profileModel.findById(profileId).session(session);
+
+        if (profile) {
+            await postModel.deleteMany({ _id: { $in: profile.posts } }).session(session);
+
+            await articleModel.deleteMany({ _id: { $in: profile.articles } }).session(session);
+
+            await Promise.all(profile.communities.map(async (communityId) => {
+                await communityModel.updateOne(
+                    { _id: communityId },
+                    { $pull: { members: userId } }
+                ).session(session);
+            }));
+
+
+            await profileModel.findByIdAndDelete(profileId).session(session);
+        }
+        await userModel.findByIdAndDelete(userId).session(session);
+
+        await session.commitTransaction();
+        session.endSession();
+
+        res.json({ message: "User account deleted successfully" });
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+
+
+module.exports = router;
+
+
+
